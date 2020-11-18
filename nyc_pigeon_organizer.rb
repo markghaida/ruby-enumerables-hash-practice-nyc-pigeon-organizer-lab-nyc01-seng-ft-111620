896d9ceacb3eeeacb9ce891aac23c438885cfa7b
Mark G <markghaida@gmail.com>
2020-11-18 13:56:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,38 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
- final_results = 
- 
- data.each_with_object({}) do |(key, value), final_array|
-	
+final_results = 
+
+data.each_with_object({}) do |(key, value), final_array| 
+
+#enumerates over the whole hash and separates the main key from the inner hash pair.  It also creates an array called final_array.
 	value.each do |inner_key, names| 
+	  #separates the inner key and the name array 
+	 
 		names.each do |name|
+		  #analyzes each element in the array of inner value
+		 
 			if !final_array[name]
+			  #if the name (e.g. Theo) is not there...
+		
 			final_array[name] = {}
+			#Im creating this: {theo => {}} 
+			#create the name Theo inside of the hash 
+			
 		end
+		
 		if !final_array[name][key]
-		      !final_array[name][key] = []
+		  
+		  #if {theo => {color => []}}
+		  
+		      final_array[name][key] = [] 
+		      #we are setting {theo => {color=> to equal [an empty array]}
+		      #final_array[name][key].push(inner_key.to_s)
+		   
     end
 			final_array[name][key].push(inner_key.to_s)
+
 		end
 	end
 end
